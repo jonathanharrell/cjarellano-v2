2c0c772e8000616499ff3a340a204612877f019b
Jonathan Harrell <harr041@gmail.com>
2021-05-30 15:14:13 -0400

    admin test

--- pages/admin.js
+++ pages/admin.js
@@ -20,11 +20,7 @@ function Admin() {
   }, []);
 
   return (
-    <div>
-      <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </Head>
-    </div>
+    <div/>
   );
 }
 
