9764a04e36379932d7f69b44903f106c547791d2
Jonathan Harrell <harr041@gmail.com>
2021-06-07 20:04:56 -0400

    wip

--- components/meta.js
+++ components/meta.js
@@ -32,7 +32,6 @@ const Meta = ({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff00fe"/>
         <meta name="msapplication-TileColor" content="#ff00fe"/>
         <meta name="theme-color" content="#ffffff"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
