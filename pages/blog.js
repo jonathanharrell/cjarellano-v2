7bda317256a1442858767dfeb1d89a72576fa080
jonathanharrell <harr041@gmail.com>
2021-12-11 16:30:06 -0500

    Widen blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { withRouter } from "next/router";
+import {withRouter} from "next/router";
 import Link from "next/link";
 import Meta from "../components/meta";
 import PostTeaser from "../components/post-teaser";
 import {getAllPosts, getAllPostTags} from "../lib/api";
-import { attributes } from "../content/blog.md";
+import {attributes} from "../content/blog.md";
 import Tag from "../components/tag";
 
 class Blog extends React.Component {
   static async getInitialProps() {
     const posts = await getAllPosts();
     const tags = await getAllPostTags();
-    return { posts, tags };
+    return {posts, tags};
   }
 
   render() {
-    const { title } = attributes;
-    const { posts, tags, router } = this.props;
+    const {title} = attributes;
+    const {posts, tags, router} = this.props;
     const tag = router.query.tag;
 
     let filteredPosts = posts;
@@ -30,34 +30,32 @@ class Blog extends React.Component {
         />
         <div className="py-28 lg:py-32">
           <div className="container">
-            <div className="max-w-4xl mx-auto">
-              <header className="mb-10 md:mb-12 lg:mb-14">
-                <Link href="/blog">
-                  <a className="inline-block">
-                    <h1 className="text-4xl md:text-5xl font-bold leading-none">
-                      {title}
-                    </h1>
-                  </a>
-                </Link>
-                <section className="mt-6">
-                  <h2 className="sr-only">Post tags</h2>
-                  {tags.map(t => (
-                      <Tag key={t} tag={t} selected={t === tag} />
+            <header className="mb-10 md:mb-12 lg:mb-14">
+              <Link href="/blog">
+                <a className="inline-block">
+                  <h1 className="text-4xl md:text-5xl font-bold leading-none">
+                    {title}
+                  </h1>
+                </a>
+              </Link>
+              <section className="mt-6">
+                <h2 className="sr-only">Post tags</h2>
+                {tags.map(t => (
+                  <Tag key={t} tag={t} selected={t === tag}/>
+                ))}
+              </section>
+            </header>
+            {filteredPosts.length > 0 ? (
+              <div className="space-y-6">
+                <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                  {filteredPosts.map(post => (
+                    <PostTeaser key={post.slug} post={post}/>
                   ))}
-                </section>
-              </header>
-              {filteredPosts.length > 0 ? (
-                <div className="space-y-6">
-                  <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-                    {filteredPosts.map(post => (
-                        <PostTeaser key={post.slug} post={post}/>
-                    ))}
-                  </div>
                 </div>
-              ) : (
-                <p className="text-2xl">No posts were found with this tag</p>
-              )}
-            </div>
+              </div>
+            ) : (
+              <p className="text-2xl">No posts were found with this tag</p>
+            )}
           </div>
         </div>
       </>
